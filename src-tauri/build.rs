16b79a6fad3e3b7d02688908322ac51fe6da0218
notochen <46807914+NotoChen@users.noreply.github.com>
2026-08-04 19:35:14 +0800

    fix: 修复 Windows 测试程序装载

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -1,3 +1,20 @@
 fn main() {
-    tauri_build::build()
+    let attributes = tauri_build::Attributes::new();
+    #[cfg(windows)]
+    let attributes = {
+        embed_windows_app_manifest();
+        attributes.windows_attributes(tauri_build::WindowsAttributes::new_without_app_manifest())
+    };
+
+    tauri_build::try_build(attributes).expect("failed to run Tauri build script");
+}
+
+#[cfg(windows)]
+fn embed_windows_app_manifest() {
+    let manifest =
+        std::path::Path::new(env!("CARGO_MANIFEST_DIR")).join("windows-app-manifest.xml");
+
+    println!("cargo:rerun-if-changed={}", manifest.display());
+    println!("cargo:rustc-link-arg=/MANIFEST:EMBED");
+    println!("cargo:rustc-link-arg=/MANIFESTINPUT:{}", manifest.display());
 }
